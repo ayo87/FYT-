2c17bcae9a42d5eab646593704b6531e75a2774a
ayo87 <aliexe419@gmail.com>
2025-09-10 11:51:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'lady bella',
+  packname: 'FYT MD',
   author: 'snowbird',
-  botName: "Lady Bella",
-  botOwner: 'Snowbird', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol.
+  botName: "FYT MD",
+  botOwner: 'ayo', // Your name
+  ownerNumber: '2347078389784', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
