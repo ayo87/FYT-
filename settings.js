f6f8bdb456ce3126677d00ec6e25a8efc83218a0
ayo87 <aliexe419@gmail.com>
2025-09-10 11:47:54 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'lady bella',
+  author: 'snowbird',
+  botName: "Lady Bella",
+  botOwner: 'Snowbird', // Your name
+  ownerNumber: '263780145644', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.0",
+};
+
+module.exports = settings;
